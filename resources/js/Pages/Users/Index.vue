b92f8057187917607a53565524e1ebf0f4761e5c
Hanafi_dev <hanafiahtkj@gmail.com>
2024-04-12 10:25:29 +0800

    #

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -144,9 +144,9 @@ onMounted(() => {
                             <Link
                                 :href="route('users.create')"
                                 :preserve-state="true"
-                                class="btn btn-primary btn-icon-square-sm"
-                                ><i class="fas fa-plus-circle"></i
-                            ></Link>
+                                class="btn btn-primary"
+                                ><i class="fas fa-plus-circle"></i> Tambah</Link
+                            >
                         </div>
                     </div>
                     <!--end card-header-->
--- resources/js/Pages/Warehouses/Index.vue
+++ resources/js/Pages/Warehouses/Index.vue
@@ -139,9 +139,9 @@ onMounted(() => {
                             <Link
                                 :href="route('warehouses.create')"
                                 :preserve-state="true"
-                                class="btn btn-primary btn-icon-square-sm"
-                                ><i class="fas fa-plus-circle"></i
-                            ></Link>
+                                class="btn btn-primary"
+                                ><i class="fas fa-plus-circle"></i> Tambah</Link
+                            >
                         </div>
                     </div>
                     <!--end card-header-->
